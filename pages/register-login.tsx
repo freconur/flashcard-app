34028245336ab52b774c4b70decdbea36caaf27d
franco condori <freconur@gmail.com>
2023-07-10 23:09:00 -0500

    agregrnado estilos al register-login

--- pages/register-login.tsx
+++ pages/register-login.tsx
@@ -6,6 +6,7 @@ import { getApp } from 'firebase/app'
 import initAuth from '../initAuth'
 import { app, authApp } from '../firebase/firebase.config'
 import { AuthAction, withAuthUser } from 'next-firebase-auth'
+import style from '../styles/RegisterLogin.module.css'
 // Note that next-firebase-auth inits Firebase for us,
 // so we don't need to.
 initAuth()
@@ -41,18 +42,38 @@ const Auth = () => {
     }
   }, [])
   return (
-    <div>
-      {renderAuth ? (
-        <StyledFirebaseAuth
-          uiConfig={firebaseAuthConfig}
-          firebaseAuth={getAuth(app)}
+    <div className='flex h-[100vh]'>
+      <div className={style.division}>
+      {/* <div className='h-[100vh] grid place-content-center p-20 w-[50%] bg-principal rounded-r-lg'> */}
+        <div className='text-slate-200'>
+          <div>
+            <h1 className='text-3xl font-semibold capitalize'>flashcard study</h1>
+          </div>
+          <p className='text-2xl font-semibold'>Te damos la bienvenida a una aplicacion simple pero efectiva para estudio </p>
+          <ul className='mt-3 text-xl font-semibold grid gap-3'>
+            <li>
+              crea y estudia flashcards
+            </li>
+            <li>
+              organizate y enfocate
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className=' bg-principal grid place-content-center w-[80%]'>
+        <h3 className='pl-5 capitalize text-2xl font-semibold text-slate-200'>inicia sesion</h3>
+        {renderAuth ? (
+          <StyledFirebaseAuth
+            uiConfig={firebaseAuthConfig}
+            firebaseAuth={getAuth(app)}
           // firebaseAuth={firebase.auth()}
-        />
-      ) : null}
+          />
+        ) : null}
+      </div>
     </div>
   )
 }
 export default withAuthUser({
   // whenUnauthedAfterInit:AuthAction.REDIRECT_TO_APP
-  whenAuthed:AuthAction.REDIRECT_TO_APP
+  whenAuthed: AuthAction.REDIRECT_TO_APP
 })(Auth)
