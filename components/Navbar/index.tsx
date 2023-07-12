bfd27ef36b7780f0db575788feebdaea8508e473
franco condori <freconur@gmail.com>
2023-07-12 15:44:25 -0500

    corrigiendo el logo

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -9,10 +9,11 @@ import LogoWeb from '../../assets/web-logo.png'
 
 const Navbar = () => {
   return (
-    <nav className='w-full  bg-principal h-[60px] flex shadow-md justify-between p-4'>
-      <div className="text-white font-semibold">
-      <Image src={LogoWeb} layout="responsive"  alt="logo-web"/>
-      </div>
+    <nav className='w-full  bg-principal h-[60px] flex shadow-md justify-between pr-1'>
+      <Link href="/" className="text-white font-semibold">
+      {/* <Image className="w-full" src={LogoWeb} layout="responsive"  alt="logo-web"/> */}
+      <Image className="p-2" src={LogoWeb} width={200} height={50}  alt="logo-web"/>
+      </Link>
       <div className="flex gap-1 items-center justify-center">
         <Link href="/register-login">
           <div className="bg-pink-400 text-white text-lg font-semibold w-[150px] h-[45px] flex items-center justify-center capitalize rounded-lg hover:bg-pink-300 duration-300 cursor-pointer">inicia sesion</div>
